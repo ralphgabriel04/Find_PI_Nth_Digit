2fca5a3453028e8d6627df98051be2a50b8580e8
Ralph Christian Gabriel <ralph_christian.gabriel@hotmail.com>
2024-11-14 10:52:39 -0500

    Create pi_decimal_generator.py
    First version working. Need to find a way to not round the number up

---
+++ pi_decimal_generator.py
@@ -0,0 +1,14 @@
+# Stocker la variable PI avec toutes ses décimal
+# Laisser l'utilisateur écrire le nb de décimal il veut
+# Afficher la variable PI selon le nombre de décimal que la personne voudra -> C'est sûrement une fonction qui a un paramètre
+
+pi = 3.14159265358979323846264338327950288419716939937510
+
+
+
+nb_decimal_voulu = int(input("Quelle est le nombre de décimal voulu pour PI ? "))
+
+
+    
+
+print(round(pi,nb_decimal_voulu))
